1857e66086974b6fcbc85b4e49d6a096b3a90e0e
Russell Gordon <rgordon@lcs.on.ca>
2024-04-13 20:13:53 -0400

    Initial Commit

---
+++ TodoWithArray/TodoWithArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoWithArrayApp.swift
+//  TodoWithArray
+//
+//  Created by Russell Gordon on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct TodoWithArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
